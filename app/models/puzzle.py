300cae8dad78b6fe68e603df3b324752cf2e1307
Weixi He <heweixiguge@gmai.com>
2025-01-21 16:21:24 -0500

    update puzzle.py

--- app/models/puzzle.py
+++ app/models/puzzle.py
@@ -4,18 +4,24 @@ from typing import TYPE_CHECKING
 
 class Puzzle(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    level: Mapped[int]
-    content: Mapped[str]
+    word: Mapped[str] 
+    definition: Mapped[str] 
+    grade: Mapped[str]
 
     progress: Mapped[list['UserProgress']] = relationship('UserProgress', back_populates='puzzle')
 
     def to_dict(self):
         return {
             'id': self.id,
-            'level': self.level,
-            'content': self.content
+            'word': self.word,
+            'definition': self.definition,
+            'grade': self.grade
         }
 
     @classmethod
     def from_dict(cls, data):
-        return Puzzle(level=data['level'], content=data['content'])
+        return Puzzle(
+            word=data['word'],
+            definition=data['definition'],
+            grade=data['grade']
+        )
